2164d617b935efa3a2fb6b33071278818ccd6615
exaFLOPs <baikdenny@gmail.com>
2025-05-13 15:29:38 +0900

    oculus droid version 0`

--- source/isaaclab/isaaclab/devices/oculus/oculus_droid.py
+++ source/isaaclab/isaaclab/devices/oculus/oculus_droid.py
@@ -532,21 +532,22 @@ class Oculus_mobile(DeviceBase):
     #    [-0.643948 , -0.756171 , -0.116345 ,  0.0297576],
     #    [ 0.       ,  0.       ,  0.       ,  1.       ]])}, {'A': False, 'B': False, 'RThU': True, 'RJ': False, 'RG': False, 'RTr': False, 'X': False, 'Y': False, 'LThU': True, 'LJ': False, 'LG': False, 'LTr': False, 'leftJS': (0.0, 0.0), 'leftTrig': (0.0,), 'leftGrip': (0.0,), 'rightJS': (0.0, 0.0), 'rightTrig': (0.0,), 'rightGrip': (0.0,)})
 
-class VRPolicy:
+class Oculus_droid(DeviceBase):
     def __init__(
         self,
-        right_controller: bool = True,
-        max_lin_vel: float = 1,
-        max_rot_vel: float = 1,
-        max_gripper_vel: float = 1,
-        spatial_coeff: float = 1,
-        pos_action_gain: float = 5,
-        rot_action_gain: float = 2,
-        gripper_action_gain: float = 3,
-        rmat_reorder: list = [-2, -1, -3, 4],
+        max_lin_vel=1,
+        max_rot_vel=1,
+        max_gripper_vel=1,
+        spatial_coeff=1,
+        pos_action_gain=5,
+        rot_action_gain=2,
+        gripper_action_gain=3,
+        rmat_reorder=[-2, -1, -3, 4],
     ):
         self.oculus_reader = OculusReader()
-        self.vr_to_global_mat = np.eye(4)
+        self.vr_to_global_mat = {"r": np.eye(4), "l": np.eye(4)}
+        self.global_to_env_mat = vec_to_reorder_mat(rmat_reorder)
+
         self.max_lin_vel = max_lin_vel
         self.max_rot_vel = max_rot_vel
         self.max_gripper_vel = max_gripper_vel
@@ -554,194 +555,116 @@ class VRPolicy:
         self.pos_action_gain = pos_action_gain
         self.rot_action_gain = rot_action_gain
         self.gripper_action_gain = gripper_action_gain
-        self.global_to_env_mat = vec_to_reorder_mat(rmat_reorder)
-        self.controller_id = "r" if right_controller else "l"
-        self.reset_orientation = True
+
+        self.reset_orientation = {"r": True, "l": True}
         self.reset_state()
 
-        # Start State Listening Thread #
         run_threaded_command(self._update_internal_state)
 
     def reset_state(self):
         self._state = {
             "poses": {},
-            "buttons": {"A": False, "B": False, "X": False, "Y": False},
-            "movement_enabled": False,
-            "controller_on": True,
+            "buttons": {},
+            "movement_enabled": {"r": False, "l": False},
+            "controller_on": {"r": True, "l": True},
         }
-        self.update_sensor = True
-        self.reset_origin = True
-        self.robot_origin = None
-        self.vr_origin = None
-        self.vr_state = None
+        self.update_sensor = {"r": True, "l": True}
+        self.reset_origin = {"r": True, "l": True}
+        self.robot_origin = {"r": None, "l": None}
+        self.vr_origin = {"r": None, "l": None}
+        self.vr_state = {"r": None, "l": None}
 
     def _update_internal_state(self, num_wait_sec=5, hz=50):
         last_read_time = time.time()
         while True:
-            # Regulate Read Frequency #
             time.sleep(1 / hz)
-
-            # Read Controller
-            time_since_read = time.time() - last_read_time
             poses, buttons = self.oculus_reader.get_transformations_and_buttons()
-            self._state["controller_on"] = time_since_read < num_wait_sec
-            if poses == {}:
-                continue
+            time_since_read = time.time() - last_read_time
+            for cid in ["r", "l"]:
+                self._state["controller_on"][cid] = time_since_read < num_wait_sec
 
-            # Determine Control Pipeline #
-            toggled = self._state["movement_enabled"] != buttons[self.controller_id.upper() + "G"]
-            self.update_sensor = self.update_sensor or buttons[self.controller_id.upper() + "G"]
-            self.reset_orientation = self.reset_orientation or buttons[self.controller_id.upper() + "J"]
-            self.reset_origin = self.reset_origin or toggled
+                if cid + "G" in buttons:
+                    toggled = self._state["movement_enabled"][cid] != buttons[cid + "G"]
+                    self.update_sensor[cid] = self.update_sensor[cid] or buttons[cid + "G"]
+                    self.reset_orientation[cid] = self.reset_orientation[cid] or buttons[cid + "J"]
+                    self.reset_origin[cid] = self.reset_origin[cid] or toggled
+                    self._state["movement_enabled"][cid] = buttons[cid + "G"]
 
-            # Save Info #
+                if cid in poses and self.reset_orientation[cid]:
+                    rot_mat = np.asarray(poses[cid])
+                    try:
+                        rot_mat = np.linalg.inv(rot_mat)
+                    except:
+                        rot_mat = np.eye(4)
+                        self.reset_orientation[cid] = True
+                    self.vr_to_global_mat[cid] = rot_mat
+                    if buttons.get(cid + "J") or self._state["movement_enabled"][cid]:
+                        self.reset_orientation[cid] = False
+
             self._state["poses"] = poses
             self._state["buttons"] = buttons
-            self._state["movement_enabled"] = buttons[self.controller_id.upper() + "G"]
-            self._state["controller_on"] = True
             last_read_time = time.time()
 
-            # Update Definition Of "Forward" #
-            stop_updating = self._state["buttons"][self.controller_id.upper() + "J"] or self._state["movement_enabled"]
-            if self.reset_orientation:
-                rot_mat = np.asarray(self._state["poses"][self.controller_id])
-                if stop_updating:
-                    self.reset_orientation = False
-                # try to invert the rotation matrix, if not possible, then just use the identity matrix
-                try:
-                    rot_mat = np.linalg.inv(rot_mat)
-                except:
-                    print(f"exception for rot mat: {rot_mat}")
-                    rot_mat = np.eye(4)
-                    self.reset_orientation = True
-                self.vr_to_global_mat = rot_mat
-
-    def _process_reading(self):
-        rot_mat = np.asarray(self._state["poses"][self.controller_id])
-        rot_mat = self.global_to_env_mat @ self.vr_to_global_mat @ rot_mat
+    def _process_reading(self, cid):
+        rot_mat = np.asarray(self._state["poses"][cid])
+        rot_mat = self.global_to_env_mat @ self.vr_to_global_mat[cid] @ rot_mat
         vr_pos = self.spatial_coeff * rot_mat[:3, 3]
         vr_quat = rmat_to_quat(rot_mat[:3, :3])
-        vr_gripper = self._state["buttons"]["rightTrig" if self.controller_id == "r" else "leftTrig"][0]
-
-        self.vr_state = {"pos": vr_pos, "quat": vr_quat, "gripper": vr_gripper}
+        trig_key = "rightTrig" if cid == "r" else "leftTrig"
+        vr_gripper = self._state["buttons"].get(trig_key, [0])[0]
+        self.vr_state[cid] = {"pos": vr_pos, "quat": vr_quat, "gripper": vr_gripper}
 
     def _limit_velocity(self, lin_vel, rot_vel, gripper_vel):
-        """Scales down the linear and angular magnitudes of the action"""
-        lin_vel_norm = np.linalg.norm(lin_vel)
-        rot_vel_norm = np.linalg.norm(rot_vel)
-        gripper_vel_norm = np.linalg.norm(gripper_vel)
-        if lin_vel_norm > self.max_lin_vel:
-            lin_vel = lin_vel * self.max_lin_vel / lin_vel_norm
-        if rot_vel_norm > self.max_rot_vel:
-            rot_vel = rot_vel * self.max_rot_vel / rot_vel_norm
-        if gripper_vel_norm > self.max_gripper_vel:
-            gripper_vel = gripper_vel * self.max_gripper_vel / gripper_vel_norm
+        lin_vel = lin_vel * min(1, self.max_lin_vel / (np.linalg.norm(lin_vel) + 1e-6))
+        rot_vel = rot_vel * min(1, self.max_rot_vel / (np.linalg.norm(rot_vel) + 1e-6))
+        gripper_vel = np.clip(gripper_vel, -self.max_gripper_vel, self.max_gripper_vel)
         return lin_vel, rot_vel, gripper_vel
 
-    def _calculate_action(self, state_dict, include_info=False):
-        # Read Sensor #
-        if self.update_sensor:
-            self._process_reading()
-            self.update_sensor = False
+    def _calculate_arm_action(self, cid, state_dict):
+        if self.update_sensor[cid]:
+            self._process_reading(cid)
+            self.update_sensor[cid] = False
 
-        # Read Observation
         robot_pos = np.array(state_dict["cartesian_position"][:3])
         robot_euler = state_dict["cartesian_position"][3:]
         robot_quat = euler_to_quat(robot_euler)
         robot_gripper = state_dict["gripper_position"]
 
-        # Reset Origin On Release #
-        if self.reset_origin:
-            self.robot_origin = {"pos": robot_pos, "quat": robot_quat}
-            self.vr_origin = {"pos": self.vr_state["pos"], "quat": self.vr_state["quat"]}
-            self.reset_origin = False
+        if self.reset_origin[cid]:
+            self.robot_origin[cid] = {"pos": robot_pos, "quat": robot_quat}
+            self.vr_origin[cid] = {"pos": self.vr_state[cid]["pos"], "quat": self.vr_state[cid]["quat"]}
+            self.reset_origin[cid] = False
 
-        # Calculate Positional Action #
-        robot_pos_offset = robot_pos - self.robot_origin["pos"]
-        target_pos_offset = self.vr_state["pos"] - self.vr_origin["pos"]
-        pos_action = target_pos_offset - robot_pos_offset
+        pos_action = (self.vr_state[cid]["pos"] - self.vr_origin[cid]["pos"]) - (
+            robot_pos - self.robot_origin[cid]["pos"]
+        )
 
-        # Calculate Euler Action #
-        robot_quat_offset = quat_diff(robot_quat, self.robot_origin["quat"])
-        target_quat_offset = quat_diff(self.vr_state["quat"], self.vr_origin["quat"])
-        quat_action = quat_diff(target_quat_offset, robot_quat_offset)
+        quat_action = quat_diff(
+            quat_diff(self.vr_state[cid]["quat"], self.vr_origin[cid]["quat"]),
+            quat_diff(robot_quat, self.robot_origin[cid]["quat"]),
+        )
         euler_action = quat_to_euler(quat_action)
 
-        # Calculate Gripper Action #
-        gripper_action = (self.vr_state["gripper"] * 1.5) - robot_gripper
+        gripper_action = (self.vr_state[cid]["gripper"] * 1.5) - robot_gripper
 
-        # Calculate Desired Pose #
-        target_pos = pos_action + robot_pos
-        target_euler = add_angles(euler_action, robot_euler)
-        target_cartesian = np.concatenate([target_pos, target_euler])
-        target_gripper = self.vr_state["gripper"]
-
-        # Scale Appropriately #
         pos_action *= self.pos_action_gain
         euler_action *= self.rot_action_gain
         gripper_action *= self.gripper_action_gain
+
         lin_vel, rot_vel, gripper_vel = self._limit_velocity(pos_action, euler_action, gripper_action)
+        return np.concatenate([lin_vel, rot_vel, [gripper_vel]])
 
-        # Prepare Return Values #
-        info_dict = {"target_cartesian_position": target_cartesian, "target_gripper_position": target_gripper}
-        action = np.concatenate([lin_vel, rot_vel, [gripper_vel]])
-        action = action.clip(-1, 1)
-
-        # Return #
-        if include_info:
-            return action, info_dict
-        else:
-            return action
+    def advance(self, obs_dict):
+        if not self._state["poses"]:
+            return np.zeros(14)
+        action_r = self._calculate_arm_action("r", obs_dict["right_arm"])
+        action_l = self._calculate_arm_action("l", obs_dict["left_arm"])
+        return np.concatenate([action_l, action_r])  # [left_arm_action | right_arm_action]
 
     def get_info(self):
         return {
-            "success": self._state["buttons"]["A"] if self.controller_id == 'r' else self._state["buttons"]["X"],
-            "failure": self._state["buttons"]["B"] if self.controller_id == 'r' else self._state["buttons"]["Y"],
+            "success": self._state["buttons"].get("A", False),
+            "failure": self._state["buttons"].get("B", False),
             "movement_enabled": self._state["movement_enabled"],
             "controller_on": self._state["controller_on"],
-        }
-
-    def forward(self, obs_dict, include_info=False):
-        if self._state["poses"] == {}:
-            action = np.zeros(7)
-            if include_info:
-                return action, {}
-            else:
-                return action
-        return self._calculate_action(obs_dict["robot_state"], include_info=include_info)
-    
-class OculusDroid(DeviceBase):
-    def __init__(self, **kwargs):
-        # Right arm: uses right controller
-        self.right_arm_policy = VRPolicy(right_controller=True, **kwargs)
-        # Left arm: uses left controller
-        self.left_arm_policy = VRPolicy(right_controller=False, **kwargs)
-
-    def advance(self, obs_dict, include_info=False):
-        # Assume robot_state contains keys: 'left_arm' and 'right_arm'
-        left_obs = {"robot_state": obs_dict["left_arm"]}
-        right_obs = {"robot_state": obs_dict["right_arm"]}
-
-        # Get actions
-        right_action, right_info = self.right_arm_policy.forward(right_obs, include_info=True)
-        left_action, left_info = self.left_arm_policy.forward(left_obs, include_info=True)
-
-        # Concatenate actions: [left_arm (7), right_arm (7)] → total 14-dim action
-        action = np.concatenate([left_action, right_action])
-
-        if include_info:
-            info = {
-                "left_arm": left_info,
-                "right_arm": right_info,
-                "left_meta": self.left_arm_policy.get_info(),
-                "right_meta": self.right_arm_policy.get_info(),
-            }
-            return action, info
-        else:
-            return action
-
-    def get_info(self):
-        return {
-            "left_arm": self.left_arm_policy.get_info(),
-            "right_arm": self.right_arm_policy.get_info(),
         }
