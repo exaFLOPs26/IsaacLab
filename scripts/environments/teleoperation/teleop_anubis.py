1. Fix gripper command 2. Base movement direction wrong speed too fast 3. Arm rotation is not working as it is expected

scripts/environments/teleoperation/teleop_anubis.py
@@ -13,7 +13,7 @@ parser.add_argument(
     "--disable_fabric", action="store_true", default=False, help="Disable fabric and use USD I/O operations."
 )
 parser.add_argument("--num_envs", type=int, default=1, help="Number of environments to simulate.")
-parser.add_argument("--teleop_device", type=str, default="keyboard_bmm", help="Device for interacting with environment")
+parser.add_argument("--teleop_device", type=str, default="oculus_droid", help="Device for interacting with environment")
 parser.add_argument("--task", type=str, default="Cabinet-anubis-teleop-v0", help="Name of the task.")
 parser.add_argument("--sensitivity", type=float, default=1.0, help="Sensitivity factor.")
 # append AppLauncher cli args
@@ -51,10 +51,6 @@ def get_ee_state(env, ee_name, gripper_value=0.0):
     ee = env.scene[ee_name].data
     pos = ee.target_pos_source[0, 0]
     rot = ee.target_quat_source[0, 0]
-
-    euler = torch.from_numpy(
-        Rotation.from_quat(rot.cpu().numpy()).as_euler('xyz')
-    ).to(dtype=rot.dtype, device=rot.device)
     
     # Gripper
     if ee_name == "ee_L_frame":
@@ -62,7 +58,7 @@ def get_ee_state(env, ee_name, gripper_value=0.0):
     else:
         body_pos = env.scene._articulations['robot'].data.body_pos_w[0, -4:-2]
     gripper_dist = torch.norm(body_pos[0] - body_pos[1])*-1*20.8+0.05 # To match [0.05, -1.65] the real robot
-    return torch.cat((pos, euler, gripper_dist.unsqueeze(0))).unsqueeze(0)
+    return torch.cat((pos, rot, gripper_dist.unsqueeze(0))).unsqueeze(0)
 
 def pre_process_actions_abs(env, abs_pose_L: torch.Tensor, gripper_command_L: bool, abs_pose_R, gripper_command_R: bool, delta_pose_base) -> torch.Tensor:
     """Pre-process actions for the environment."""
@@ -133,16 +129,16 @@ def pre_process_actions(delta_pose_L: torch.Tensor, gripper_command_L: bool, del
         
         # resolve gripper command
         gripper_vel_L = torch.zeros(delta_pose_L.shape[0], 1, device=delta_pose_L.device)
-        gripper_vel_L[:] = -1.0 if gripper_command_L else 1.0
+        gripper_vel_L[:] = 1.0 if gripper_command_L else -1.0
 
         gripper_vel_R = torch.zeros(delta_pose_R.shape[0], 1, device=delta_pose_R.device)
-        gripper_vel_R[:] = -1.0 if gripper_command_R else 1.0
+        gripper_vel_R[:] = 1.0 if gripper_command_R else -1.0
         
         # TODO Check if wheel_radius is for real wheels or the cylinders inside the wheels
         delta_pose_base_wheel = compute_wheel_velocities_torch(
             delta_pose_base[:, 0], delta_pose_base[:, 1], delta_pose_base[:, 2],
             wheel_radius=0.103, l=0.05
-        ) * 100 # Shape: (batch_size, 3)
+        ) * 10 # Shape: (batch_size, 3)
         delta_pose_base_wheel = delta_pose_base_wheel[:, [2, 1, 0]]
 
         # Ensure gripper velocities and base poses have the correct shapes  

source/isaaclab/isaaclab/devices/oculus/oculus_droid.py
@@ -417,14 +417,12 @@ class Oculus_droid(DeviceBase):
         # state_dict: shape [num_envs, 8]
         num_envs = state_dict.shape[0]
         actions = []
-
         if self.update_sensor[cid]:
             self._process_reading(cid)
             self.update_sensor[cid] = False
 
         for env_idx in range(num_envs):
             single_state = state_dict[env_idx]
-
             robot_pos = single_state[:3].cpu().numpy()
             robot_quat = single_state[3:7].cpu().numpy()
             robot_gripper = single_state[7].item()
@@ -459,60 +457,62 @@ class Oculus_droid(DeviceBase):
         print(f"Callback for {button_name} registered.")
     
     def advance(self, obs_dict):
-        
+        num_envs = obs_dict["right_arm"].shape[0]
+        action_l = np.zeros((num_envs, 7))  # [num_envs, 7] for left arm
+        action_r = np.zeros((num_envs, 7))  # [num_envs, 7] for right arm
+        action_base = np.zeros((num_envs,3))  
+
         if self._state["poses"] == {}:
             return (
-            np.zeros(6),  # pose_L
-            0.0,          # gripper_command_L
-            np.zeros(6),  # pose_R
-            0.0,          # gripper_command_R
-            np.zeros(3),  # delta_pose_base
-        )
+            action_l[:, :6],  # pose_L
+            action_l[:, 6],   # gripper_command_L
+            action_r[:, :6],  # pose_R
+            action_r[:, 6],   # gripper_command_R
+            action_base,      # delta_pose_base
+            ) 
         
         # Arm
         if self._state["movement_enabled"]["L"]:
             action_l = self._calculate_arm_action("L", obs_dict["left_arm"])
-            action_l[:3] *= self.pos_action_gain
-            action_l[3:6] *= self.rot_action_gain
-            action_l[6] *= 1.0       
+            action_l[:, :3] *= self.pos_action_gain
+            action_l[:, 3:6] *= self.rot_action_gain
+            action_l[:, 6] *= 1.0     
         else:
-            action_l = np.zeros(7)
+            action_l = np.zeros((num_envs, 7)) 
 
         if self._state["movement_enabled"]["R"]:
             action_r = self._calculate_arm_action("R", obs_dict["right_arm"])
-            action_r[:3] *= self.pos_action_gain
-            action_r[3:6] *= self.rot_action_gain
-            action_r[6] *= 1.0
+            action_r[:, :3] *= self.pos_action_gain
+            action_r[:, 3:6] *= self.rot_action_gain
+            action_r[:, 6] *= 1.0
         else:
-            action_r = np.zeros(7)
-        
-        action_base = np.zeros(3)  
-        
+            action_r = np.zeros((num_envs, 7)) 
+         
         # Base
         
         # raw rotation
-        if buttons['rightJS'][0] < -0.7  and ('counterclockwise' not in self._key_hold_start):
-            action_base += self.base_sensitivity * self.base_rot_sensitivity
+        if self._state["buttons"]['rightJS'][0] < -0.7:
+            action_base[:, 2] += self.base_sensitivity * self.base_rot_sensitivity
 
         # check if the rightJS is moved to left
-        elif buttons['rightJS'][0] > 0.7 and ('clockwise' not in self._key_hold_start):
-            action_base -= self.base_sensitivity * self.base_rot_sensitivity
+        elif self._state["buttons"]['rightJS'][0] > 0.7:
+            action_base[:, 2] -= self.base_sensitivity * self.base_rot_sensitivity
 
-        elif buttons['rightJS'][0] == 0.0:
-                action_base = 0.0
+        elif self._state["buttons"]['rightJS'][0] == 0.0:
+                action_base[:, 2] = 0.0
         
         # xy
-        raw_x, raw_y = buttons['leftJS']
-        action_base[0] = raw_x * self.base_sensitivity
-        action_base[1] = raw_y * self.base_sensitivity
+        raw_x, raw_y = self._state["buttons"]['leftJS']
+        action_base[:, 0] = raw_x * self.base_sensitivity
+        action_base[:, 1] = raw_y * self.base_sensitivity
         
         
         return (
-        action_l[:6],  # pose_L
-        action_l[6],   # gripper_command_L
-        action_r[:6],  # pose_R
-        action_r[6],   # gripper_command_R
-        action_base,   # delta_pose_base
+        action_l[:, :6],  # pose_L
+        action_l[:, 6],   # gripper_command_L
+        action_r[:, :6],  # pose_R
+        action_r[:, 6],   # gripper_command_R
+        action_base,      # delta_pose_base
         )    
 
     def advance_onearm(self, obs_dict):