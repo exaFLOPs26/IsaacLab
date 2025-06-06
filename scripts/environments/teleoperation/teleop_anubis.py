small mass for dummy spheres were the problem 1. Fix rotation

scripts/environments/teleoperation/teleop_anubis.py
@@ -46,9 +46,10 @@ from isaaclab_tasks.manager_based.manipulation.lift import mdp
 from isaaclab_tasks.utils import parse_env_cfg
 from scipy.spatial.transform import Rotation
 
-def get_ee_state(env, ee_name, gripper_value=0.0):
+def get_ee_state(env, ee_name):
     # arm
     ee = env.scene[ee_name].data
+    # ipdb.set_trace()
     pos = ee.target_pos_source[0, 0]
     rot = ee.target_quat_source[0, 0]
     
@@ -131,7 +132,6 @@ def pre_process_actions(delta_pose_L: torch.Tensor, gripper_command_L: bool, del
         # Ensure commands are tensors
         gripper_command_L = torch.as_tensor(gripper_command_L, device=delta_pose_L.device).unsqueeze(-1) 
         gripper_command_R = torch.as_tensor(gripper_command_R, device=delta_pose_R.device).unsqueeze(-1) 
-        print(f"gripper_command_R: {gripper_command_R}")
         # Create output tensors
         # TODO Check if wheel_radius is for real wheels or the cylinders inside the wheels
         delta_pose_base_wheel = compute_wheel_velocities_torch(
@@ -239,13 +239,13 @@ def main():
         # run everything in inference mode
         with torch.inference_mode():
             if args_cli.teleop_device.lower() == "oculus_droid":
-                # Left arm
-                ee_l_state = get_ee_state(env, "ee_L_frame", gripper_value=0.0)
-                ee_r_state = get_ee_state(env, "ee_R_frame", gripper_value=0.0)
+                # Get End-Effector states
+                ee_l_state = get_ee_state(env, "ee_L_frame")
+                ee_r_state = get_ee_state(env, "ee_R_frame")
 
                 obs_dict = {"left_arm": ee_l_state, "right_arm": ee_r_state}
 
-                pose_L, gripper_command_L, pose_R, gripper_command_R, delta_pose_base = teleop_interface.advance(obs_dict)
+                pose_L, gripper_command_L, pose_R, gripper_command_R, delta_pose_base = teleop_interface.advance(env, obs_dict)
 
             else:
                 # ipdb.set_trace()

source/isaaclab/isaaclab/devices/oculus/oculus_droid.py
@@ -282,8 +282,8 @@ class Oculus_droid(DeviceBase):
         max_rot_vel=1,
         max_gripper_vel=1,
         spatial_coeff=1,
-        pos_action_gain=0.08,
-        rot_action_gain=0.008,
+        pos_action_gain=0.1,
+        rot_action_gain=0.00001,
         gripper_action_gain=0.03,
         base_sensitivity=0.4,
         base_rot_sensitivity=15,
@@ -402,7 +402,7 @@ class Oculus_droid(DeviceBase):
 
     def _limit_velocity(self, lin_vel, rot_vel, gripper_vel):
         lin_vel = lin_vel * min(1, self.max_lin_vel / (np.linalg.norm(lin_vel) + 1e-6))
-        rot_vel = rot_vel * min(1, self.max_rot_vel / (np.linalg.norm(rot_vel) + 1e-6))
+        # rot_vel = rot_vel * min(1, self.max_rot_vel / (np.linalg.norm(rot_vel) + 1e-6))
 
         # gripper_vel = np.clip(gripper_vel, -self.max_gripper_vel, self.max_gripper_vel)
         
@@ -420,7 +420,7 @@ class Oculus_droid(DeviceBase):
             single_state = state_dict[env_idx]
             robot_pos = single_state[:3].cpu().numpy()
             robot_quat = single_state[3:7].cpu().numpy()
-            robot_gripper = single_state[7].item()
+            # robot_gripper = single_state[7].item()
 
             if self.reset_origin[cid]:
                 self.robot_origin[cid] = {"pos": robot_pos, "quat": robot_quat}
@@ -431,16 +431,20 @@ class Oculus_droid(DeviceBase):
                 robot_pos - self.robot_origin[cid]["pos"]
             )
 
+            print("vr", Rotation.from_quat(quat_diff(self.vr_state[cid]["quat"], self.vr_origin[cid]["quat"])).as_rotvec())
             quat_action = quat_diff(
                 quat_diff(self.vr_state[cid]["quat"], self.vr_origin[cid]["quat"]),
                 quat_diff(robot_quat, self.robot_origin[cid]["quat"]),
             )
-            euler_action = Rotation.from_quat(quat_action).as_rotvec()
-
+            
+            rotvec_action = Rotation.from_quat(quat_action).as_rotvec()
+            # print("rotvec_action", rotvec_action)
+            
             gripper_action = -1.0 if self.vr_state[cid]["gripper"] > 0.5 else self.vr_state[cid]["gripper"]
 
-            lin_vel, rot_vel, gripper_vel = self._limit_velocity(pos_action, euler_action, gripper_action)
+            lin_vel, rot_vel, gripper_vel = self._limit_velocity(pos_action, rotvec_action, gripper_action)
             action = np.concatenate([lin_vel, rot_vel, [gripper_vel]])
+
             actions.append(action)
 
         return np.stack(actions)  # shape: [num_envs, 7]
@@ -451,7 +455,7 @@ class Oculus_droid(DeviceBase):
         self._additional_callbacks[button_name] = func
         print(f"Callback for {button_name} registered.")
     
-    def advance(self, obs_dict):
+    def advance(self, env, obs_dict):
         num_envs = obs_dict["right_arm"].shape[0]
         action_l = np.zeros((num_envs, 7))  # [num_envs, 7] for left arm
         action_r = np.zeros((num_envs, 7))  # [num_envs, 7] for right arm
@@ -465,7 +469,7 @@ class Oculus_droid(DeviceBase):
             action_r[:, 6],   # gripper_command_R
             action_base,      # delta_pose_base
             ) 
-        
+
         # Arm
         if self._state["movement_enabled"]["L"]:
             action_l = self._calculate_arm_action("L", obs_dict["left_arm"])
@@ -484,7 +488,8 @@ class Oculus_droid(DeviceBase):
             action_r = np.zeros((num_envs, 7)) 
          
         # Base
-        
+        # env.scene.cfg.robot.spawn.articulation_props.fix_root_link = True does not work
+
         # raw rotation
         if self._state["buttons"]['rightJS'][0] < -0.7:
             action_base[:, 2] += self.base_sensitivity * self.base_rot_sensitivity
@@ -495,13 +500,12 @@ class Oculus_droid(DeviceBase):
 
         elif self._state["buttons"]['rightJS'][0] == 0.0:
                 action_base[:, 2] = 0.0
-        
+                
         # xy
         raw_x, raw_y = self._state["buttons"]['leftJS']
         action_base[:, 1] = raw_x * self.base_sensitivity
         action_base[:, 0] = raw_y * self.base_sensitivity * (-1)
-        
-        
+            
         return (
         action_l[:, :6],  # pose_L
         action_l[:, 6],   # gripper_command_L